b7b66e546766aee8bfe84fbd701c3541feed1ffe
George Surridge <surridge.g@icloud.com>
2025-11-20 11:49:50 +0000

    fix pomodoro timer deployment structure

--- projects/pomodoro-timer/vite.config.ts
+++ projects/pomodoro-timer/vite.config.ts
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/projects/pomodoro-timer/',
+  build: {
+    outDir: '.',
+    emptyOutDir: false,
+  },
   plugins: [react(), tailwindcss()],
 })
